ac0e0ba6798bf62fee1404db5f9c027ff847699a
Waine Tam <wainetam@gmail.com>
2015-01-27 18:28:02 -0500

    initial commit

---
+++ BlocBrowser/BlocBrowser/WebBrowserViewController.h
@@ -0,0 +1,13 @@
+//
+//  WebBrowserViewController.h
+//  BlocBrowser
+//
+//  Created by Waine Tam on 1/27/15.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WebBrowserViewController : UIViewController
+
+@end
